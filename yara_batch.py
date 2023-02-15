a96b098ee92bcf3d97329326e4b526bef2c42e28
Winsome <38833067+Winsome21@users.noreply.github.com>
2023-02-15 17:17:29 -0600

    Update yara_batch.py

--- yara_batch.py
+++ yara_batch.py
@@ -13,27 +13,26 @@ def main():
     args = get_args()
     data = open(args.file, 'r').read()
     output = args.output
-    rules = findall('(?:^private\srule|^rule).+?^\}', data, multiline|DOTALL)
+    rules = findall('(?:^private\srule|^rule).+?condition:.+?^\}', data, multiline|DOTALL)
 
     num_rules = len(rules)
     batch_size = args.batch
     blocks = math.ceil(num_rules / batch_size)
     count_rules = 0
-    print('Total rules: {}'.format(num_rules))
-    print('Batch size: {}'.format(batch_size))
-    print('Number of blocks: {}'.format(blocks))
-    for i in range(0, blocks):
+    print(f'Total rules: {num_rules}')
+    print(f'Batch size: {batch_size}')
+    print(f'Number of blocks: {blocks}')
+    for i in range(blocks):
         rule_ready = ''
         count_rules_block = 0
         for rule in rules[i*batch_size:(i+1)*batch_size]:
             count_rules += 1
             count_rules_block += 1
             rule_ready += rule + '\n\n'
-        f1 = open('{}\\batch_{}.yara'.format(output, i+1), 'w')
-        f1.write(rule_ready)
-        f1.close()
-        print('{} rules written to {}\\batch_{}.yara'.format(count_rules_block, output, i+1))
-    print('Total rules written: {}'.format(count_rules))
+        with open(f'{output}\\batch_{i + 1}.yara', 'w') as f1:
+            f1.write(rule_ready)
+        print(f'{count_rules_block} rules written to {output}\\batch_{i + 1}.yara')
+    print(f'Total rules written: {count_rules}')
 
 if __name__ == '__main__':
     main()
